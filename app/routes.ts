7f0ef1861c8c1adafa7fbe54d0d5a9d8ecc1b809
destiny <desto4q@gmail.com>
2025-05-26 05:15:56 +0100

    middleware and routing base

---
+++ app/routes.ts
@@ -0,0 +1,15 @@
+// import { type RouteConfig, index } from "@react-router/dev/routes";
+
+// export default [index("routes/home.tsx")] satisfies RouteConfig;
+
+import { type RouteConfig } from "@react-router/dev/routes";
+import { nextRoutes, appRouterStyle } from "rr-next-routes/react-router";
+// for remix use this instead
+// import {nextRoutes, appRouterStyle} from "rr-next-routes/remix";
+
+const routes = nextRoutes({
+  print: "info",
+  routeFileNames: ["page", "index", "route"],
+});
+
+export default routes satisfies RouteConfig;
