b0d6a2d31e8069c21e0d6e13f1cab70e35315b33
destiny <desto4q@gmail.com>
2026-01-31 21:35:44 +0100

    partner sign up password validation

--- src/api/apiClient.tsx
+++ src/api/apiClient.tsx
@@ -1,5 +1,5 @@
 import type { TypedPocketBase } from "pocketbase-types";
 import { PocketBaseTS } from "pocketbase-ts";
 const url = import.meta.env.VITE_API_URL;
-const local_url = "http://127.0.0.1:8090/";
-export let pb = new PocketBaseTS(local_url) as Partial<TypedPocketBase>;
+// const local_url = "http://127.0.0.1:8090/";
+export let pb = new PocketBaseTS(url) as Partial<TypedPocketBase>;
